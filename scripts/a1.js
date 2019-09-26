af8432d255415490684ef44e47bc9b9e84ce8303
gopal97-beep <55570231+gopal97-beep@users.noreply.github.com>
2019-09-26 12:11:57 +0530

    Add files via upload

---
+++ scripts/a1.js
@@ -0,0 +1,62 @@
+var tt1="",tt2="",tt3="",tt4="",tt5="",tt6="",tt7="",tt8="";
+var pt1="",pt2="",pt3="",pt4="",pt5="",pt6="",pt7="",pt8=""
+var g=[];
+var f=[];
+function add1(){
+     tt1="The Alchemist";
+     pt1="$150";
+     g+=tt1+"<br>";
+     f+=pt1+"<br>";
+    
+
+}
+function add2(){
+     tt2="The Great Gatsby";
+     pt2="$180";
+    g+=tt2+"<br>";
+    f+=pt2+"<br>";
+
+}
+function add3(){
+     tt3="1984";
+     pt3="$200";
+    g+=tt3+"<br>";
+    f+=pt3+"<br>";
+
+}
+function add4(){
+     tt4="The Old Man and the Sea";
+     pt4="$200";
+    g+=tt4+"<br>";
+    f+=pt4+"<br>";
+
+}
+function add5(){
+     tt5="The Hobbit  There and Back Again";
+    g+=tt5+"<br>";
+
+}
+function add6(){
+    tt5="Murder on the Orient Express";
+   g+=tt6+"<br>";
+
+}
+function add7(){
+    tt5="The Little Prince";
+   g+=tt7+"<br>";
+
+}
+
+function add8(){
+    tt5="The Metamorphosis";
+   g+=tt8+"<br>";
+
+}
+function cart() 
+{   for(var i=0;i<9;i++){
+    
+}
+    document.getElementById("lol2").innerHTML+="<tr><td>"+g+""+"</td><td>"+f+"</td></tr>";
+}
+
+
